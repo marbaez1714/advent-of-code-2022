4435ba3fcae73b437c507c2cb75336f0befcb0d0
Mario Baez <mario.baez@dialexa.com>
2022-12-03 13:09:35 -0600

    day 1 done

--- src/day-1/index.ts
+++ src/day-1/index.ts
@@ -5,25 +5,48 @@ import path from "path";
 /* Utils                      */
 /******************************/
 const getData = async () => {
-  return await fs.readFile(path.resolve(__dirname, "./input.txt"), {
-    encoding: "utf8",
-  });
+  return await fs
+    .readFile(path.resolve(__dirname, "./input.txt"), {
+      encoding: "utf8",
+    })
+    .then((data) =>
+      data
+        .split("\n\n")
+        .map((arr) => arr.split("\n").map((item) => parseInt(item)))
+    );
 };
 
 /******************************/
 /* Part One                   */
 /******************************/
 export const partOne = async () => {
-  const answer = "answer";
+  // Turn data into array of arrays of numbers
+  const data = await getData();
 
-  return answer;
+  // Get all of the totals
+  const totals = data.reduce<number[]>((acc, curr) => {
+    const total = curr.reduce((a, b) => a + b, 0);
+    return [...acc, total];
+  }, []);
+
+  // return the max value
+  return Math.max(...totals);
 };
 
 /******************************/
 /* Part Two                   */
 /******************************/
 export const partTwo = async () => {
-  const answer = "answer";
+  // Turn data into array of arrays of numbers
+  const data = await getData();
 
-  return answer;
+  // Get all of the totals sorted
+  const totals = data
+    .reduce<number[]>((acc, curr) => {
+      const total = curr.reduce((a, b) => a + b, 0);
+      return [...acc, total];
+    }, [])
+    .sort((a, b) => b - a);
+
+  return totals[0] + totals[1] + totals[2];
 };
